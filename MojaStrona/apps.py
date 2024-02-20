40e0ca3bba87d7c1a2d8beefa6f60c80a649075f
ShamanKNG <kyrtap2205@gmail.com>
2024-02-20 17:01:03 +0100

    Initial commit

---
+++ MojaStrona/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MojastronaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'MojaStrona'
---
+++ MojaStrona/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def wszystkie(request):
+    return HttpResponse("Hello World")
